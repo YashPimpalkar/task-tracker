4dcf719d4d3558bbd147a2ed12b6b43f0d378171
Yash Pimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2025-04-26 03:02:42 +0530

    Update api.js

--- task-tracker/src/services/api.js
+++ task-tracker/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api",
+  // baseURL: "http://localhost:8081/api",
+   baseURL: "https://backend-task-2xag.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
